ad34e7f300ffd58e6384eef0bee57355477ea133
bryanpadao <bryanpadao@gmail.com>
2018-09-04 22:57:55 +0800

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  showChart(){
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                   'width':400,
+                   'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+
+  }
+}
